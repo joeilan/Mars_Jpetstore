77af8bc695753ecbcf60645a420f6bbffe2088e4
Ilangathir Joseph <joeilan@hotmail.com>
2022-11-01 21:48:59 +0530

    3 commit

--- mars-jpetstore/src/test/java/driver/BrowserFactory.java
+++ mars-jpetstore/src/test/java/driver/BrowserFactory.java
@@ -2,8 +2,10 @@ package driver;
 
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserFactory {
@@ -12,14 +14,20 @@ public class BrowserFactory {
 
     public WebDriver getBrowser() {
 
-                    
+                 
+    	FirefoxOptions firefoxOptions = new FirefoxOptions();
+    	WebDriverManager.firefoxdriver().setup();
+    	return new FirefoxDriver(firefoxOptions);
+    	
+    	
+    	
                 	
-                	ChromeOptions chromeOptions = new ChromeOptions();
+               /* 	ChromeOptions chromeOptions = new ChromeOptions();
         			WebDriverManager.chromedriver().setup();
         		        		
         			return new ChromeDriver(chromeOptions);
                 	
-
+              */
             }
         
     }
